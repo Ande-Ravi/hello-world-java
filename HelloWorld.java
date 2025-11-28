91d4fc195a2a1fbd8bdbb440a5b458a21104fb51
raviande <anderavi.iiit@gmail.com>
2025-11-28 16:18:40 +0530

    Changes for Poll SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -37,7 +37,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, DevOps_with_Ravi");
     System.out.println("Hello, Jenkins_Hello_World_Job");
-    System.out.println("Hello, Jenkins");
+    System.out.println("Hello, Jenkins_Workflow");
 
   }
 }
